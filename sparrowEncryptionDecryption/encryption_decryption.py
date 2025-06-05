91b3b9665661b95e092dcd3affa296e09cc45581
DY282 <2821963166@qq.com>
2025-06-05 15:12:10 +0800

    修改主入口不可调用压缩算法的问题

--- sparrowEncryptionDecryption/encryption_decryption.py
+++ sparrowEncryptionDecryption/encryption_decryption.py
@@ -15,7 +15,7 @@ class SparrowEncryptionDecryption:
         self._decryption_ = SparrowDecryption(order_keys1, order_keys2, easy_keys1, easy_keys2)
 
     def order_encryption(self, string: str, key: str, effective_duration: int = -1, is_compression: int = 2,
-                         mode: int = 0):
+                         mode: int = 0, compression_type: str = None):
         """
         加密数据
         :param string: 需要被加密的数据
@@ -23,53 +23,57 @@ class SparrowEncryptionDecryption:
         :param effective_duration: 秘钥过期时间，-1为永不过期
         :param is_compression: 默认为2，二次压缩压缩，1为一次压缩，0为不压缩
         :param mode: 加密模式，0为二进制加密，1为四进制加密
+        :param compression_type: 压缩算法(zlib、gzip、bz2、lzma、lz4、brotli、snappy、huffman、deflate、lz77)
         :return: 返回被加密好的数据
         """
         if string == "" or string is None:
             raise SparrowInputDataNoneError
         if key == "" or key is None:
             raise SparrowInputDataNoneError
-        return self._encryption_.order_encryption(string, key, effective_duration, is_compression, mode)
+        return self._encryption_.order_encryption(string, key, effective_duration, is_compression, mode, compression_type)
 
-    def easy_encryption(self, string: str, key: str, mode: int = 0):
+    def easy_encryption(self, string: str, key: str, mode: int = 0, compression_type: str = None):
         """
         加密数据
         :param string: 需要被加密的数据
         :param key: 秘钥
         :param mode: 加密模式，0为二进制加密，1为四进制加密
+        :param compression_type: 压缩算法(zlib、gzip、bz2、lzma、lz4、brotli、snappy、huffman、deflate、lz77)
         :return: 返回被加密好的数据
         """
         if string == "" or string is None:
             raise SparrowInputDataNoneError
         if key == "" or key is None:
             raise SparrowInputDataNoneError
-        return self._encryption_.easy_encryption(string, key, mode)
+        return self._encryption_.easy_encryption(string, key, mode, compression_type)
 
-    def order_decryption(self, decompression: str, key: str):
+    def order_decryption(self, decompression: str, key: str, compression_type: str = None):
         """
         将被加密的数据解密
         :param decompression: 需要被解密的数据
         :param key: 秘钥
+        :param compression_type: 压缩算法(zlib、gzip、bz2、lzma、lz4、brotli、snappy、huffman、deflate、lz77)
         :return: 返回被解密的数据或秘钥错误类型
         """
         if decompression == "" or decompression is None:
             raise SparrowInputDataNoneError
         if key == "" or key is None:
             raise SparrowInputDataNoneError
-        return self._decryption_.order_decryption(decompression, key)
+        return self._decryption_.order_decryption(decompression, key, compression_type)
 
-    def easy_decryption(self, decompression: str, key: str):
+    def easy_decryption(self, decompression: str, key: str, compression_type: str = None):
         """
         将被加密的数据解密
         :param decompression: 需要被解密的数据
         :param key: 秘钥
+        :param compression_type: 压缩算法(zlib、gzip、bz2、lzma、lz4、brotli、snappy、huffman、deflate、lz77)
         :return: 返回被解密的数据或秘钥错误类型
         """
         if decompression == "" or decompression is None:
             raise SparrowInputDataNoneError
         if key == "" or key is None:
             raise SparrowInputDataNoneError
-        return self._decryption_.easy_decryption(decompression, key)
+        return self._decryption_.easy_decryption(decompression, key, compression_type)
 
 
 class SparrowEncryptionDecryptionAsync(SparrowEncryptionDecryption):
@@ -79,35 +83,35 @@ class SparrowEncryptionDecryptionAsync(SparrowEncryptionDecryption):
         self._decryption_ = SparrowDecryptionAsync(order_keys1, order_keys2, easy_keys1, easy_keys2)
 
     async def order_encryption(self, string: str, key: str, effective_duration: int = -1, is_compression: int = 2,
-                               mode: int = 0):
+                               mode: int = 0, compression_type: str = None):
         """
         异步加密数据。
         """
         loop = asyncio.get_event_loop()
         return await loop.run_in_executor(None, partial(
             super().order_encryption, string=string, key=key, effective_duration=effective_duration,
-            is_compression=is_compression, mode=mode))
+            is_compression=is_compression, mode=mode, compression_type=compression_type))
 
-    async def easy_encryption(self, string: str, key: str, mode: int = 0):
+    async def easy_encryption(self, string: str, key: str, mode: int = 0, compression_type: str = None):
         """
         异步简单加密数据。
         """
         loop = asyncio.get_event_loop()
         return await loop.run_in_executor(None, partial(
-            super().easy_encryption, string=string, key=key, mode=mode))
+            super().easy_encryption, string=string, key=key, mode=mode, compression_type=compression_type))
 
-    async def order_decryption(self, decompression: str, key: str):
+    async def order_decryption(self, decompression: str, key: str, compression_type: str = None):
         """
         异步解密数据。
         """
         loop = asyncio.get_event_loop()
         return await loop.run_in_executor(None, partial(
-            super().order_decryption, decompression=decompression, key=key))
+            super().order_decryption, decompression=decompression, key=key, compression_type=compression_type))
 
-    async def easy_decryption(self, decompression: str, key: str):
+    async def easy_decryption(self, decompression: str, key: str, compression_type: str = None):
         """
         异步简单解密数据。
         """
         loop = asyncio.get_event_loop()
         return await loop.run_in_executor(None, partial(
-            super().easy_decryption, decompression=decompression, key=key))
+            super().easy_decryption, decompression=decompression, key=key, compression_type=compression_type))
